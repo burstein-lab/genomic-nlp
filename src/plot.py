1bbea9b7d6a941b0dfb38a409862d5b8ad2224f1
daniellemiller <danimillers10@gmail.com>
2022-06-01 14:26:52 +0300

    support average AUPRs with sd and plot by fold for LOTGOCV

--- src/plot.py
+++ src/plot.py
@@ -7,6 +7,9 @@ from sklearn import metrics
 import numpy as np
 import pickle
 
+import matplotlib
+matplotlib.rcParams['pdf.fonttype'] = 42
+matplotlib.rcParams['ps.fonttype'] = 42
 
 class ModelPlots(object):
     def __init__(self, mdl):
@@ -125,6 +128,57 @@ class FoldModelPlots(object):
                 pdf.savefig(transparent=True, bbox_inches="tight")
                 plt.close()
 
+    def plot_roc_by_fold(self):
+        roc_df = self.roc_data
+        res = self.mdl_report
+
+        roc_df = roc_df[roc_df['class'] != 'ALL']
+        roc_df['fold'] = roc_df['fold'].apply(lambda x: x.split('_')[-1])
+
+        n_folds = roc_df["fold"].nunique()
+        folds = list(roc_df["fold"].unique())
+        COLS = 3
+        ROWS = math.ceil(n_folds / COLS)
+        pages = math.ceil(ROWS / 3)
+
+        class2color = {'Amino sugar and nucleotide sugar metabolism':'DarkOrchid',
+                       'Benzoate degradation':'darkorange', 'Energy metabolism':'cornflowerblue', 'Other':'grey',
+                       'Oxidative phosphorylation':'gold',
+                       'Porphyrin and chlorophyll metabolism':'teal',
+                       'Prokaryotic defense system':'tomato', 'Ribosome':'deeppink', 'Secretion system':'pink',
+                       'Two-component system':'turquoise', 'ALL':'k'}
+
+        with PdfPages(os.path.join(self.outdir, f'{self.name}_ROC_BY_FOLD.pdf')) as pdf:
+            for page in range(pages):
+                fig, ax = plt.subplots(COLS, COLS, figsize=(20,16))
+
+                i, j = 0, 0
+                for fold in folds[page*COLS*COLS: page*COLS*COLS +COLS*COLS]:
+                    d = roc_df[roc_df["fold"] == fold]
+
+                    for cl in d["class"].unique():
+                        ax[i][j].plot(d[d["class"] == cl]["fpr"], d[d["class"] == cl]["tpr"],
+                                      lw=3, label=cl, color=class2color[cl])
+                        ax[i][j].plot([0, 1], [0, 1], color='grey', lw=3, linestyle='--', alpha=0.2)
+                        ax[i][j].set(xlim=[-0.05, 1.0], ylim=[0.0, 1.05], title=f"{fold}")
+                        ax[i][j].grid(True)
+                        ax[i][j].text(.6, 0.05, "AUC:{0:0.2f}".format(res.groupby('fold')['auc'].mean().loc[f'fold_{fold}']),
+                                      fontsize=12)
+                        if j == 0:
+                            ax[i][j].set(ylabel="True Positive Rate")
+                        if i == COLS - 1:
+                            ax[i][j].set(xlabel="False Positive Rate")
+                    if j == (COLS-1):
+                        j = 0
+                        i += 1
+                    else:
+                        j += 1
+                plt.subplots_adjust(hspace=.3, wspace=.3)
+                _ = [ax[k][q].axis("off") for k in range(COLS) for q in range(COLS) if not ax[k][q].lines]
+                pdf.savefig(transparent=True, bbox_inches="tight")
+                plt.close()
+
+
     def plot_precision_recall(self):
         pr_df = self.precision_data
         res = self.mdl_report
@@ -171,31 +225,97 @@ class FoldModelPlots(object):
                 pdf.savefig(transparent=True, bbox_inches="tight")
                 plt.close()
 
+
+    def plot_precision_recall_by_fold(self):
+        pr_df = self.precision_data
+        res = self.mdl_report
+
+        pr_df = pr_df[pr_df['class'] != 'ALL']
+        pr_df['fold'] = pr_df['fold'].apply(lambda x: x.split('_')[-1])
+
+        n_folds = pr_df["fold"].nunique()
+        folds = list(pr_df["fold"].unique())
+        COLS = 3
+        ROWS = math.ceil(n_folds / COLS)
+        pages = math.ceil(ROWS / 3)
+
+        class2color = {'Amino sugar and nucleotide sugar metabolism':'DarkOrchid',
+                       'Benzoate degradation':'darkorange', 'Energy metabolism':'cornflowerblue', 'Other':'grey',
+                       'Oxidative phosphorylation':'gold',
+                       'Porphyrin and chlorophyll metabolism':'teal',
+                       'Prokaryotic defense system':'tomato', 'Ribosome':'deeppink', 'Secretion system':'pink',
+                       'Two-component system':'turquoise', 'ALL':'k'}
+
+
+        with PdfPages(os.path.join(self.outdir, f'{self.name}_AUPR_BY_FOLD.pdf')) as pdf:
+            for page in range(pages):
+                fig, ax = plt.subplots(COLS, COLS, figsize=(20,16))
+
+                i, j = 0, 0
+                for fold in folds[page*COLS*COLS : COLS*COLS*(page +1)]:
+
+                    d = pr_df[pr_df["fold"] == fold]
+                    d = d[~((d['precision'] == 0) & (d['recall'] == 0))]
+
+                    for cl in d["class"].unique():
+                        ax[i][j].plot(d[d["class"] == cl]["recall"], d[d["class"] == cl]["precision"], lw=3,
+                                      label=cl, color=class2color[cl]
+                                      )
+                        ax[i][j].set(xlim=[-0.05, 1.0], ylim=[0.0, 1.05], title=f"{fold}")
+                        ax[i][j].grid(True)
+                        ax[i][j].text(0.01, 0.05, "AUPR:{0:0.2f}".format(res.groupby('fold')['aupr'].mean().loc[f'fold_{fold}']),
+                                      fontsize=12)
+                        if j == 0:
+                            ax[i][j].set(ylabel="Precision")
+                        if i == COLS - 1:
+                            ax[i][j].set(xlabel="Recall")
+
+                    if j == (COLS-1):
+                        j = 0
+                        i += 1
+                    else:
+                        j += 1
+                plt.subplots_adjust(hspace=.3, wspace=.3)
+                _ = [ax[k][q].axis("off") for k in range(COLS) for q in range(COLS) if not ax[k][q].lines]
+                pdf.savefig(transparent=True, bbox_inches="tight")
+                plt.close()
+
+
     def plot_single_aupr_with_ci(self):
         data = self.mdl.folds_pr
+        overall_data = self.mdl.mean_pr
+        overall_data = overall_data[~((overall_data['precision'] == 0) & (overall_data['recall'] == 0))]
         class2aupr = {}
-        colors = ['pink', 'turquoise', 'darkorange', 'cornflowerblue', 'teal', 'gold', 'olive','tomato', 'deeppink']
+        class2color = {'Amino sugar and nucleotide sugar metabolism':'DarkOrchid',
+                       'Benzoate degradation':'darkorange', 'Energy metabolism':'cornflowerblue', 'Other':'grey',
+                       'Oxidative phosphorylation':'gold',
+                       'Porphyrin and chlorophyll metabolism':'teal',
+                       'Prokaryotic defense system':'tomato', 'Ribosome':'deeppink', 'Secretion system':'pink',
+                       'Two-component system':'turquoise', 'ALL':'k'}
+
 
         with PdfPages(os.path.join(self.outdir, f'{self.name}_AUPR_CI.pdf')) as pdf:
             fig, ax = plt.subplots(figsize=(5, 4))
-            for c, cl in zip(cycle(colors), data["class"].unique()):
-                if cl == "ALL":
-                    c = "k"
+            for cl in data["class"].unique():
+
                 class_data = data[data["class"] == cl]
                 tprs = class_data['interp'].tolist()[0]
-                mean_tpr = np.mean(tprs, axis=0)
-                mean_fpr = np.linspace(0, 1, 100)
-                mean_auc = metrics.auc(mean_fpr, mean_tpr)
+                mean_precision = np.mean(tprs, axis=0)
+                mean_recall = np.linspace(0, 1, mean_precision.shape[0])
+                mean_auc = metrics.auc(mean_recall, mean_precision)
+
+                if cl == 'ALL':
+                    ax.plot(mean_recall, mean_precision, color=class2color[cl], label="{0} ({1:0.2f})".format(cl, mean_auc), lw=3, alpha=.8)
+                else:
+                    pr_data = overall_data[overall_data['class'] == cl]
+                    mean_auc = metrics.auc(pr_data['recall'], pr_data['precision'])
+                    ax.plot(pr_data['recall'], pr_data['precision'], color=class2color[cl],label="{0} ({1:0.2f})".format(cl, mean_auc), lw=3, alpha=.8)
+
+                    mean_precision = np.mean(tprs)
+                    std_precision = np.std(tprs, axis=0)
+                    ax.fill_between(pr_data['recall'], pr_data['precision'] + std_precision, pr_data['precision'] - std_precision, color=class2color[cl], alpha=.1)
+
                 class2aupr[cl] = mean_auc
-
-                ax.plot(mean_fpr, mean_tpr, color=c,
-                        label="{0} ({1:0.2f})".format(cl, mean_auc), lw=3, alpha=.8)
-
-                std_tpr = np.std(tprs, axis=0)
-                tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
-                tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
-                ax.fill_between(mean_fpr, tprs_lower, tprs_upper, color=c, alpha=.1)
-
             ax.set(xlim=[-0.05, 1.0], ylim=[0.0, 1.05], title=f"AUPR", xlabel="Recall", ylabel="Precision")
             ax.grid(True)
             plt.legend(bbox_to_anchor=(1.01, 1))
@@ -207,26 +327,30 @@ class FoldModelPlots(object):
     def plot_single_roc_with_ci(self):
         data = self.mdl.folds_roc
 
-        colors = ['pink', 'turquoise', 'darkorange', 'cornflowerblue', 'teal', 'gold', 'olive','tomato', 'deeppink']
+        class2color = {'Amino sugar and nucleotide sugar metabolism':'DarkOrchid',
+                       'Benzoate degradation':'darkorange', 'Energy metabolism':'cornflowerblue', 'Other':'grey',
+                       'Oxidative phosphorylation':'gold',
+                       'Porphyrin and chlorophyll metabolism':'teal',
+                       'Prokaryotic defense system':'tomato', 'Ribosome':'deeppink', 'Secretion system':'pink',
+                       'Two-component system':'turquoise', 'ALL':'k'}
 
         with PdfPages(os.path.join(self.outdir, f'{self.name}_ROC_CI.pdf')) as pdf:
             fig, ax = plt.subplots(figsize=(5, 4))
-            for c, cl in zip(cycle(colors), data["class"].unique()):
-                if cl == "ALL":
-                    c = "k"
+            for cl in data["class"].unique():
+
                 class_data = data[data["class"] == cl]
                 tprs = class_data['interp'].tolist()[0]
                 mean_tpr = np.mean(tprs, axis=0)
                 mean_fpr = np.linspace(0, 1, 100)
                 mean_auc = metrics.auc(mean_fpr, mean_tpr)
 
-                ax.plot(mean_fpr, mean_tpr, color=c,
+                ax.plot(mean_fpr, mean_tpr, color=class2color[cl],
                         label="{0} ({1:0.2f})".format(cl, mean_auc), lw=3, alpha=.8)
 
                 std_tpr = np.std(tprs, axis=0)
                 tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
                 tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
-                ax.fill_between(mean_fpr, tprs_lower, tprs_upper, color=c, alpha=.1)
+                ax.fill_between(mean_fpr, tprs_lower, tprs_upper, color=class2color[cl], alpha=.1)
 
             ax.set(xlim=[-0.05, 1.0], ylim=[0.0, 1.05], title=f"ROC", xlabel="True Positive Rate",
                    ylabel="False Positive Rate")
